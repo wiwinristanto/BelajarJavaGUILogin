379fce34fb40cab3fe87a9044814d7a847394e06
wiwinristanto <wiwinristanto94@gmail.com>
2022-01-07 05:12:48 +0700

    dev : Design Register Form

--- src/register.java
+++ src/register.java
@@ -2,4 +2,10 @@ import javax.swing.*;
 
 public class register {
     private JPanel registerPanel;
+    private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JPasswordField passwordField3;
 }
